6224c7f732a6d683b43d0d47d2941f00f3513335
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-19 20:30:52 +0900

    Send message to discord

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,72 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+type QueueMessage = UnknownMessage | SendMessage;
+
+type UnknownMessage = {
+	type: "unknown";
+};
+
+type SendMessage = {
+	type: "send_message";
+	channelId: string;
+	message: Record<string, string>;
+};
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	DQUEUE: Queue<QueueMessage>;
+	DISCORD_TOKEN: string;
+
+	// DISCORD_TARGET_CHANNEL_ID: string; // test
 }
 
+const version = "10";
+const baseURL = `https://discord.com/api/v${version}`;
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	// 	async fetch(
+	// 		request: Request,
+	// 		env: Env,
+	// 		ctx: ExecutionContext,
+	// 	): Promise<Response> {
+	// 		await env.DQUEUE.send({
+	// 			type: "send_message",
+	// 			channelId: env.DISCORD_TARGET_CHANNEL_ID,
+	// 			message: {
+	// 				content: `# Title
+	// this is title
+
+	// ## Subtitle
+	// this is subtitle
+	// `,
+	// 			},
+	// 		});
+	// 		return new Response("ok");
+	// 	},
+
+	async queue(
+		batch: MessageBatch<QueueMessage>,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<void> {
+		for (const message of batch.messages) {
+			if (message.body.type === "send_message") {
+				const body = message.body;
+				const url = `${baseURL}/channels/${body.channelId}/messages`;
+				const messageBody = body.message;
+
+				const resp = await fetch(url, {
+					method: "POST",
+					headers: {
+						Authorization: `Bot ${env.DISCORD_TOKEN}`,
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(messageBody),
+				});
+				if (resp.status !== 200) {
+					console.error(`failed: ${await resp.text()}`);
+					message.retry();
+					continue;
+				}
+			}
+			message.ack();
+		}
 	},
 };
