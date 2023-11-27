519b1472b915602d5922fc8f4082ea369fb16a55
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-28 01:12:04 +0900

    Send multi server

--- src/worker.ts
+++ src/worker.ts
@@ -10,11 +10,19 @@ type SendMessage = {
 	message: Record<string, string>;
 };
 
+type Config = {
+	DEFAULT: ConfigItem;
+	CAT_BOT: ConfigItem;
+};
+
+type ConfigItem = {
+	TOKEN: string;
+	CHANNEL_ID: string;
+};
+
 export interface Env {
 	DQUEUE: Queue<QueueMessage>;
-	DISCORD_TOKEN: string;
-
-	// DISCORD_TARGET_CHANNEL_ID: string; // test
+	DISCORD_CONFIG: string;
 }
 
 const version = "10";
@@ -49,13 +57,16 @@ export default {
 		for (const message of batch.messages) {
 			if (message.body.type === "send_message") {
 				const body = message.body;
-				const url = `${baseURL}/channels/${body.channelId}/messages`;
 				const messageBody = body.message;
 
+				const config = JSON.parse(env.DISCORD_CONFIG) as Config;
+				const token = config.CAT_BOT.TOKEN;
+				const channelId = config.CAT_BOT.CHANNEL_ID;
+				const url = `${baseURL}/channels/${channelId}/messages`;
 				const resp = await fetch(url, {
 					method: "POST",
 					headers: {
-						Authorization: `Bot ${env.DISCORD_TOKEN}`,
+						Authorization: `Bot ${token}`,
 						"Content-Type": "application/json",
 					},
 					body: JSON.stringify(messageBody),
@@ -65,6 +76,19 @@ export default {
 					message.retry();
 					continue;
 				}
+				// const resp = await fetch(url, {
+				// 	method: "POST",
+				// 	headers: {
+				// 		Authorization: `Bot ${"aaa"}`,
+				// 		"Content-Type": "application/json",
+				// 	},
+				// 	body: JSON.stringify(messageBody),
+				// });
+				// if (resp.status !== 200) {
+				// 	console.error(`failed: ${await resp.text()}`);
+				// 	message.retry();
+				// 	continue;
+				// }
 			}
 			message.ack();
 		}
